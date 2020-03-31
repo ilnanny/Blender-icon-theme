447d7f51e82d8a22ac533712c4b9fb22d7d37b5a
ilnanny <Ilary@2012>
2020-03-31 06:03:05 +0200

    Update folders colors

--- icon-cache-maker.sh
+++ icon-cache-maker.sh
@@ -8,10 +8,15 @@
 echo "  Making of icon cache for icon-theme"
 echo "==============================================="
 echo ""
-echo "  Blender"
+echo "  Blender - icon theme"
 echo "================================================"
 echo ""
-sudo gtk-update-icon-cache -f /usr/share/icons/Blender
+sudo gtk-update-icon-cache -f /usr/share/icons/blender-blue/
+sudo gtk-update-icon-cache -f /usr/share/icons/blender-cyan
+sudo gtk-update-icon-cache -f /usr/share/icons/blender-dark
+sudo gtk-update-icon-cache -f /usr/share/icons/blender-dkblue
+sudo gtk-update-icon-cache -f /usr/share/icons/blender-kaki
+sudo gtk-update-icon-cache -f /usr/share/icons/blender-red
 echo ""
 echo "================================================"
 echo "  Close your Terminal emulator"
